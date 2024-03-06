e5eac0d3b1c78abfaebd8ba8ab1cf809da4383a3
jitenderyadavofc <157213132+jitenderyadavofc@users.noreply.github.com>
2024-03-06 23:23:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 
 module "aws_lake_formation_register" {
-  source          = "./modules/lake-formation/lf-register-location"
+  source  = "app.terraform.io/my-birla-org/lakeformation/aws/lf-register-location"
+  version = "0.9.1"
+
   bucket_register = var.lf_register.bucket_arn
 }
 
 
 module "aws_lake_formation_permission" {
-  source = "./modules/lake-formation/lf-permissions"
+  source  = "app.terraform.io/my-birla-org/lakeformation/aws/lf-permissions"
+  version = "0.9.1"
 
   for_each = { for idx, details in var.lf_permissions : idx => details if length(var.lf_permissions) > 0 }
 
